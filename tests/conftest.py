f6037d5350b81eebb6fa13f1135bb8cc12e66967
Gangcai Xie <gcxiester@gmail.com>
2025-05-13 12:05:20 +0800

    add meta_file parser

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,4 @@
 
 def pytest_addoption(parser):
     parser.addoption("--test_data_dir", action="store", required=True, help="Path to root path of the .h5ad files (testing datasets)")
+    parser.addoption("--meta_file", action="store", required=True, help="Path to meta file")
