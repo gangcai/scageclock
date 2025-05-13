9e1a05fba9d825058b0768e16f1dd960119fb11d
Gangcai Xie <gcxiester@gmail.com>
2025-05-13 12:06:00 +0800

    update parameter selection

--- tests/test_scAgeClock.py
+++ tests/test_scAgeClock.py
@@ -1,33 +1,42 @@
 import pytest
-from ..scageclock.model.GatedMultiheadAttention import GatedMultiheadAttentionAgeClock
+from ..scageclock.scAgeClock import training_pipeline
 from scipy.stats import pearsonr
 
+# ["linear", "xgboost", "catboost", "MLP", "GMA"]
 @pytest.mark.parametrize(
-    "validation_during_training,predict_dataset,loader_method",
+    "model_name, validation_during_training,predict_dataset,loader_method",
     [
-        (True,"validation","scageclock"),
-        (False,"validation","scageclock"),
-        (False,"testing","scageclock"),
-        (True,"validation","scageclock_balanced"),
+        ("GMA",True,"validation","scageclock"),
+        ("linear",True,"validation","scageclock"),
+        ("xgboost",True,"validation","scageclock"),
+        ("catboost",True,"validation","scageclock"),
+        ("MLP",True,"validation","scageclock"),
     ]
 )
 
-def test_GMAAgeClock(validation_during_training,
-                         predict_dataset,
-                         loader_method,
-                         request):
+def test_training_pipeline(model_name,
+                           validation_during_training,
+                           predict_dataset,
+                           loader_method,
+                           request):
     ad_dir_root = request.config.getoption("--test_data_dir")
+    meta_file = request.config.getoption("--meta_file")
     dataset_folder_dict = {"training": "train", "validation": "val", "testing": "test"}
-    age_clock = GatedMultiheadAttentionAgeClock(anndata_dir_root=ad_dir_root,
-                                                dataset_folder_dict=dataset_folder_dict,
-                                                predict_dataset=predict_dataset,
-                                                validation_during_training=validation_during_training,
-                                                loader_method=loader_method)
-    age_clock.train()
-    predict_list = age_clock.predict()
-    corr, p_value = pearsonr(predict_list[0], predict_list[1])
-    feature_importances = age_clock.get_feature_importance()
-    test_metrics_df, y_test_pred, y_test, soma_ids_all = age_clock.cell_level_test()
-    assert len(predict_list) == 3
-    assert len(feature_importances) > 0
-    assert len(y_test_pred) > 0
+    if model_name in ["xgboost","catboost"]:
+        results = training_pipeline(model_name=model_name,
+                                    ad_dir_root=ad_dir_root,
+                                    meta_file_path=meta_file,
+                                    dataset_folder_dict=dataset_folder_dict,
+                                    predict_dataset=predict_dataset,
+                                    validation_during_training=validation_during_training,
+                                    loader_method=loader_method,
+                                    train_dataset_fully_loaded=True, )
+    else:
+        results = training_pipeline(model_name=model_name,
+                                    ad_dir_root=ad_dir_root,
+                                    meta_file_path=meta_file,
+                                    dataset_folder_dict=dataset_folder_dict,
+                                    predict_dataset=predict_dataset,
+                                    validation_during_training=validation_during_training,
+                                    loader_method=loader_method)
+    assert results == True
