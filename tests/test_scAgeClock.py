33b21442344b9baf808a193bc5c1778bd42ac270
Gangcai Xie <gcxiester@gmail.com>
2025-06-14 16:20:20 +0800

    add k-fold mode

--- tests/test_scAgeClock.py
+++ tests/test_scAgeClock.py
@@ -4,13 +4,15 @@ from scipy.stats import pearsonr
 
 # ["linear", "xgboost", "catboost", "MLP", "GMA"]
 @pytest.mark.parametrize(
-    "model_name, validation_during_training,predict_dataset,loader_method",
+    "model_name, validation_during_training,predict_dataset,loader_method,K_fold_mode",
     [
-        ("GMA",True,"validation","scageclock"),
-        ("linear",True,"validation","scageclock"),
-        ("xgboost",True,"validation","scageclock"),
-        ("catboost",True,"validation","scageclock"),
-        ("MLP",True,"validation","scageclock"),
+        ("GMA",True,"validation","scageclock",False),
+        ("linear",True,"validation","scageclock",False),
+        ("xgboost",True,"validation","scageclock",False),
+        ("catboost",True,"validation","scageclock",False),
+        ("MLP",True,"validation","scageclock",False),
+        ("GMA",False,"training","scageclock",True),
+        ("GMA",True,"training","scageclock",True),
     ]
 )
 
@@ -18,8 +20,11 @@ def test_training_pipeline(model_name,
                            validation_during_training,
                            predict_dataset,
                            loader_method,
+                           K_fold_mode,
                            request):
     ad_dir_root = request.config.getoption("--test_data_dir")
+    k_fold_data_dir = request.config.getoption("--k_fold_data_dir")
+
     meta_file = request.config.getoption("--meta_file")
     dataset_folder_dict = {"training": "train", "validation": "val", "testing": "test"}
     if model_name in ["xgboost","catboost"]:
@@ -32,11 +37,26 @@ def test_training_pipeline(model_name,
                                     loader_method=loader_method,
                                     train_dataset_fully_loaded=True, )
     else:
-        results = training_pipeline(model_name=model_name,
-                                    ad_dir_root=ad_dir_root,
-                                    meta_file_path=meta_file,
-                                    dataset_folder_dict=dataset_folder_dict,
-                                    predict_dataset=predict_dataset,
-                                    validation_during_training=validation_during_training,
-                                    loader_method=loader_method)
+        if K_fold_mode:
+            results = training_pipeline(model_name=model_name,
+                                        ad_dir_root=k_fold_data_dir,
+                                        meta_file_path=meta_file,
+                                        dataset_folder_dict=dataset_folder_dict,
+                                        predict_dataset=predict_dataset,
+                                        K_fold_mode=K_fold_mode,
+                                        K_fold_train=("Fold1", "Fold2"),
+                                        K_fold_val=("Fold3"),
+                                        validation_during_training=validation_during_training,
+                                        loader_method=loader_method)
+        else:
+            results = training_pipeline(model_name=model_name,
+                                        ad_dir_root=ad_dir_root,
+                                        meta_file_path=meta_file,
+                                        dataset_folder_dict=dataset_folder_dict,
+                                        predict_dataset=predict_dataset,
+                                        K_fold_mode=K_fold_mode,
+                                        K_fold_train=("Fold1", "Fold2"),
+                                        K_fold_val=("Fold3"),
+                                        validation_during_training=validation_during_training,
+                                        loader_method=loader_method)
     assert results == True
