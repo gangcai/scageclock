102df9e35bce45931e6d7f2908abb31e9a2d9cc2
Gangcai Xie <gcxiester@gmail.com>
2025-06-14 18:58:20 +0800

    add model file settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,6 @@ def pytest_addoption(parser):
     parser.addoption("--meta_file", action="store", required=True, help="Path to meta file")
     parser.addoption("--k_fold_data_dir", action="store", required=True, help="Path to root path of the .h5ad files (k-fold mode datasets)")
     parser.addoption("--out_root_dir", action="store", required=True, help="Path to the output")
+    parser.addoption("--model_file", action="store", required=True, help="Path to model file (such as .pth file for GMA model)")
+
 
