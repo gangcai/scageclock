8577dbda350f87d00f1d5bdcb82ff26fda840cda
Gangcai Xie <gcxiester@gmail.com>
2025-06-14 16:20:37 +0800

    add k fold mode input dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,4 @@
 def pytest_addoption(parser):
     parser.addoption("--test_data_dir", action="store", required=True, help="Path to root path of the .h5ad files (testing datasets)")
     parser.addoption("--meta_file", action="store", required=True, help="Path to meta file")
+    parser.addoption("--k_fold_data_dir", action="store", required=True, help="Path to root path of the .h5ad files (k-fold mode datasets)")
