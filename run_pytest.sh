3181461035bacc024cd0acf7cd62b711cfd22f98
Gangcai Xie <gcxiester@gmail.com>
2025-06-14 16:46:22 +0800

    add pytest running script

---
+++ run_pytest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+poetry run pytest  --k_fold_data_dir="data/pytest_data/k_fold_mode/" --test_data_dir="data/pytest_data/train_val_test_mode/" --meta_file="data/pytest_data/pytest_dataset_metadata.parquet" --out_root_dir="./tmp/"
