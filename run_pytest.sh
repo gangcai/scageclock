6521d27cc862aa3afeaddd133580a8967ac33509
Gangcai Xie <gcxiester@gmail.com>
2025-08-08 14:26:10 +0800

    optimize settings

--- run_pytest.sh
+++ run_pytest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p tmp/
 #testing for a single test file
-#poetry run pytest tests/test_evaluation.py --k_fold_data_dir="data/pytest_data/k_fold_mode/" --test_data_dir="data/pytest_data/train_val_test_mode/" --meta_file="data/pytest_data/pytest_dataset_metadata.parquet" --out_root_dir="./tmp/" --model_file="./data/trained_models/GMA_models/GMA_celltype_balanced_basicRun.pth"
+#poetry run pytest tests/test_evaluation.py --k_fold_data_dir="data/pytest_data/k_fold_mode/" --test_data_dir="data/pytest_data/train_val_test_mode/" --meta_file="data/pytest_data/pytest_dataset_metadata.parquet" --out_root_dir="./tmp/" --model_file="./data/trained_models/scAgeClock_GMA_model_state_dict.pth"
 
 #fully testing
-poetry run pytest  --k_fold_data_dir="data/pytest_data/k_fold_mode/" --test_data_dir="data/pytest_data/train_val_test_mode/" --meta_file="data/pytest_data/pytest_dataset_metadata.parquet" --out_root_dir="./tmp/" --model_file="./data/trained_models/GMA_models/GMA_celltype_balanced_basicRun.pth"
+poetry run pytest  --k_fold_data_dir="data/pytest_data/k_fold_mode/" --test_data_dir="data/pytest_data/train_val_test_mode/" --meta_file="data/pytest_data/pytest_dataset_metadata.parquet" --out_root_dir="./tmp/" --model_file="./data/trained_models/scAgeClock_GMA_model_state_dict.pth"
