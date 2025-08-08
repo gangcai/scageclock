9947e222ca536e5b2b54ad17e1b18c5cc3195bbe
Gangcai Xie <gcxiester@gmail.com>
2025-08-08 14:26:37 +0800

    update settings

--- run_scAgeClock_CLI.sh
+++ run_scAgeClock_CLI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-model_file="./data/trained_models/GMA_models/GMA_celltype_balanced_basicRun.pth"
+model_file="./data/trained_models/scAgeClock_GMA_model_state_dict.pth"
 h5ad_folder="./data/pytest_data/train_val_test_mode/test/"
 poetry run scAgeClock --model_file ${model_file} --testing_h5ad_files_dir ${h5ad_folder} --output_file './tmp/test_predicted.xlsx'
